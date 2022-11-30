3db7b8f800fdd9120777c32caa8e3c3898aa84bf
vasubawa <vasusharma2000@hotmail.com>
2022-11-29 22:30:01 -0500

    Major rewrite

---
+++ src/addressBook/Person.java
@@ -0,0 +1,32 @@
+package addressBook;
+/*
+ * EXAMPLE OF HOW ITS IN HERE:
+ * public class Restaurant {                          // Info about a restaurant
+   private String name;
+   private int rating;
+
+   public void setName(String restaurantName) {    // Sets the restaurant's name
+	  name = restaurantName;
+   }
+
+   public void setRating(int userRating) {         // Sets the rating (1-5, with 5 best)
+	  rating = userRating;
+   }
+
+   public void print() {                           // Prints name and rating on one line
+	  System.out.println(name + " -- " + rating);
+   }
+}
+ */
+public class Person {
+
+	private String address;
+	private String email;
+	private String id;
+	private String name;
+	private int phone; 
+
+	public Person(){
+
+	}
+}
