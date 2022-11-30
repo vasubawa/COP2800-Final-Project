2eefdb80601242c176a0a16a4a5be6c655026379
vasubawa <vasusharma2000@hotmail.com>
2022-11-30 16:28:11 -0500

    Added subclasses

---
+++ src/AddressBook/Faculty.java
@@ -0,0 +1,5 @@
+package AddressBook;
+
+public class Faculty extends Person{
+    
+}
--- src/AddressBook/Person.java
+++ src/AddressBook/Person.java
@@ -20,18 +20,28 @@ package AddressBook;
  */
 public class Person {
 
-	private String address;
+	private String firstName;
+	private String lastName;
 	private String email;
-	private String id;
-	private String name;// maybe seperate to first and last name?idk
-	private int phone; 
+	private int phoneNumber;
+	private String streetAddress;
+	private String city;
+	private String state;
+	private int zipcode;
+	//private String id;
 
-	public Person() {
-		address = "Unknown";
-		email = "Unknown";
-		id = "Unknown";
-		name = "Unknown";
-		phone = -1;
+	// Default Constructor
+	public Person(/*user inputs here later*/) {
+		this.firstName = "Unknown";
+		this.lastName = "Unknown";
+		this.email = "Unknown";
+		this.phoneNumber = -1;
+		this.streetAddress = "Unknown";
+		this.city = "Unknown";
+		this.state = "Unknown";
+		this.zipcode = -1;
+		//this.id = "Unknown";
 	}
+	
 	
 }
