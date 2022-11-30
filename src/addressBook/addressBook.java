dfef4df70f03909fc6cafec621c8742c0958105e
vasubawa <vasusharma2000@hotmail.com>
2022-11-29 21:04:08 -0500

    Fixed bug

--- src/addressBook/addressBook.java
+++ src/addressBook/addressBook.java
@@ -5,17 +5,23 @@ public class addressBook {
     public static void main(String[] args) throws Exception { 
 
 
-        String menuChoice;
+        char menuChoice = '\n';
         Scanner scnr = new Scanner(System.in);
         //System.out.println("Welcome to Team 2's Address Book.");
 
-        //Another testing
+        // A loop that will keep the program running as long as "X" isn't entered
         do {
-            menuChoice = userInput(scnr);
-            
-        } while (!menuChoice.equals("X"));
+            // Gets the users Input from the menu.
+            menuChoice = userInput(scnr).charAt(0);
 
-        System.out.println("\nGood-bye");
+            if (menuChoice != 'C') {
+                System.out.println("\n\tError: Please select option N from the menu first!");
+            } else {
+            }
+            // Stops the loop if "X" is entered.
+        } while (menuChoice != 'X');
+        // Prints good-bye message.
+        System.out.println("\nThank you for using Team 2's Address Book! Good-bye!");
     }
 
     public static void processChoice(){
@@ -26,8 +32,9 @@ public class addressBook {
      * 
      * @return menuChoice Returns whatever the user has inputted.
      */
-    public static String userInput(Scanner scnr) {
+    public static String menu() {
         // Declaring String Variable;
+        Scanner scnr = new Scanner(System.in);
         String menuChoice;
         // Menu options for user to choose.
         System.out.println("\nAddressBook Menu\n");
@@ -38,8 +45,10 @@ public class addressBook {
         System.out.println("E(X)it\n");
         System.out.print("Choice: ");
 
+        // Exit processing
         menuChoice = scnr.nextLine();
         menuChoice = menuChoice.toUpperCase();
+        scnr.close();
         // Returns the choice
         return menuChoice;
     }
