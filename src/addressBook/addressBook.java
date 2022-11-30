987b0a3636897fc661f73481205836e31d9fd903
vasubawa <vasusharma2000@hotmail.com>
2022-11-29 19:04:23 -0500

    Menu Class

--- src/addressBook/addressBook.java
+++ src/addressBook/addressBook.java
@@ -7,90 +7,41 @@ public class addressBook {
 
         String menuChoice;
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Welcome to Team 2's Address Book.");
+        //System.out.println("Welcome to Team 2's Address Book.");
 
         //Another testing
         do {
-
             menuChoice = userInput(scnr);
-
-            if ((!menuChoice.equals("X") && !menuChoice.equals("C") && !menuChoice.equals("R") && !menuChoice.equals("U") && !menuChoice.equals("D"))) {
-                System.out.println("\nError: Please select an option from the menu\n");
-            } else if (menuChoice.equals("X")) {
-                break;
-            } else if (menuChoice.equals("C") || menuChoice.equals("R") || menuChoice.equals("U") || menuChoice.equals("D")) {
-switch (menuChoice) {
-                    case "C":
-                        System.out.println("\n C test \n");
-                        break;
-                    case "R":
-                        System.out.println("\n R test \n");
-                        break;
-                    case "U":
-                        System.out.println("\n U test \n");
-                        break;
-                    case "D":
-                        System.out.println("\n D test \n");
-                        break;
-                }
-            }
+            
         } while (!menuChoice.equals("X"));
 
         System.out.println("\nGood-bye");
-        }
+    }
 
+    public static void processChoice(){
 
-        public static String userInput(Scanner scnr) {
+    }
+    /**
+     * Prints out the menu for the user to select from.
+     * 
+     * @return menuChoice Returns whatever the user has inputted.
+     */
+    public static String userInput(Scanner scnr) {
+        // Declaring String Variable;
+        String menuChoice;
+        // Menu options for user to choose.
+        System.out.println("\nAddressBook Menu\n");
+        System.out.println("(C)reate");
+        System.out.println("(R)etrieve");
+        System.out.println("(U)pdate");
+        System.out.println("(D)elete");
+        System.out.println("E(X)it\n");
+        System.out.print("Choice: ");
 
-            String menuChoice;
-
-            System.out.println("Please select an option from the menu below:");
-            System.out.println("(C)reate");
-            System.out.println("(R)etrieve");
-            System.out.println("(U)pdate");
-            System.out.println("(D)elete");
-            System.out.println("E(X)it");
-            System.out.print("Choice: ");
-
-            menuChoice = scnr.nextLine();
-            menuChoice = menuChoice.toUpperCase();
-
-            return menuChoice;
-
-     }
-
+        menuChoice = scnr.nextLine();
+        menuChoice = menuChoice.toUpperCase();
+        // Returns the choice
+        return menuChoice;
+    }
 }
-/* 
-public class Person {
 
-
-    private String Id;
-    private String Name;
-    private String Address;
-    private String Email;
-    private int Phone;
-
-    public Person() { 
-        setId("Faculty");
-    }
-
-    public Person() { 
-        setName("Bob Smith");
-    }
-
-    public Person() { 
-        setAddress("122 Main St");
-    }
-
-    public Person() { 
-        setEmail("bsmith122@gmail.com");
-    }
-
-    public Person() { 
-        setPhone("5328754538");
-    }
-
-
-
-}
-*/
