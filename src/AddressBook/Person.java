20d5e40419fffc06d6b2f1230ed23e0fafcad31c
vasubawa <vasusharma2000@hotmail.com>
2022-11-30 16:51:19 -0500

    Removed comments

--- src/AddressBook/Person.java
+++ src/AddressBook/Person.java
@@ -1,23 +1,5 @@
 package AddressBook;
-/*
- * EXAMPLE OF HOW ITS IN HERE:
- * public class Restaurant {                          // Info about a restaurant
-   private String name;
-   private int rating;
 
-   public void setName(String restaurantName) {    // Sets the restaurant's name
-	  name = restaurantName;
-   }
-
-   public void setRating(int userRating) {         // Sets the rating (1-5, with 5 best)
-	  rating = userRating;
-   }
-
-   public void print() {                           // Prints name and rating on one line
-	  System.out.println(name + " -- " + rating);
-   }
-}
- */
 public class Person {
 
 	private String firstName;
@@ -42,6 +24,6 @@ public class Person {
 		this.zipcode = -1;
 		//this.id = "Unknown";
 	}
-	
+
 	
 }
