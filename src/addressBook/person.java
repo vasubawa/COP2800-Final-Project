849411c844996c797021924309c8dc6a6bfa1a3e
vasubawa <vasusharma2000@hotmail.com>
2022-11-29 19:04:49 -0500

    a

---
+++ src/addressBook/person.java
@@ -0,0 +1,34 @@
+package addressBook;
+
+public class person {
+
+
+    private String Id;
+    private String Name;
+    private String Address;
+    private String Email;
+    private int Phone; 
+
+    public Person() { 
+        //setId("Faculty");
+    }
+
+    public Person() { 
+        //setName("Bob Smith");
+    }
+
+    public Person() { 
+        //setAddress("122 Main St");
+    }
+
+    public Person() { 
+        //setEmail("bsmith122@gmail.com");
+    }
+
+    public Person() { 
+        //setPhone("5328754538");
+    }
+
+
+
+}
