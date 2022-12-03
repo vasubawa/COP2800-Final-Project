1f6dfcb0b491881a019bb97742647e17a83cadec
vasubawa <vasusharma2000@hotmail.com>
2022-12-02 23:25:33 -0500

    CRUD WORKS

--- src/AddressBook/AddressBookMain.java
+++ src/AddressBook/AddressBookMain.java
@@ -1,9 +1,9 @@
 package AddressBook;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class AddressBookMain {
+
 	public static void main(String[] args) {
 
 		ArrayList<Person> list = new ArrayList<Person>();
@@ -20,89 +20,57 @@ public class AddressBookMain {
 	}
 
 	public static void processChoice(char menuChoice, ArrayList<Person> list) {
-
-		String pName = "\n";
-		String pEmail = "\n";
-		String pAddress = "\n";
-		long pPhoneNum = -1;
-		int pAge = -1;
+		// String pName = "\n";
+		// String pEmail = "\n";
+		// String pAddress = "\n";
+		// long pPhoneNum = -1;
+		// int pAge = -1;
 		int pID = -1;
-		int id = -1;
 
 		char personChoice;
 		float sGPA = -1;
 		int sID = -1;
 		String fTitle = "\n";
 		int fSalary = -1;
-
+		Scanner scnr = new Scanner(System.in);
 		boolean found = false;
 		int dID = -1;
-		Scanner sScan = new Scanner(System.in);
-		Scanner iScan = new Scanner(System.in);
 
 		Iterator<Person> iterList = list.iterator();
 		switch (menuChoice) {
 		case 'C':
 			// Person person = new Person();
-			System.out.println("Please enter the following information:");
-
-			System.out.println("Full Name:");
-			pName = "pName"; // sScan.nextLine();
-
-			System.out.println("Age:");
-			pAge = -1;// iScan.nextInt();
-
-			do {
-				System.out.println("Email:");
-				pEmail = "p@email.com"; // sScan.next();
-				if (Person.checkEmail(pEmail) == false) {
-					System.out.println("Email is not correct format.");
-				}
-			} while (Person.checkEmail(pEmail) == false);
-
-			// System.out.println("Email:");
-			// pEmail = "p@email.com";//sScan.nextLine();
-
-			System.out.println("Phone Number:");
-			pPhoneNum = -1;// sScan.nextLine();
-
-			System.out.println("Home Address:");
-			pAddress = "pAddress";// sScan.nextLine();
-
-			System.out.println("ID Number:");
-			pID = sScan.nextInt();
-
+			Person cTemp = createInput(found, list);
 			personChoice = menuTypeSelection().charAt(0);
 			switch (personChoice) {
 
 			case 'S':
-				System.out.println("Student ID:");
-				sID = iScan.nextInt();
 				System.out.println("Student GPA:");
-				sGPA = iScan.nextInt();
-				// p.add(new Student(pAge,pEmail , pAddress, pName, pPhoneNum, sID, sGPA));
+				sGPA = scnr.nextInt();
+				list.add(new Student(cTemp, sGPA));
 				break;
 
 			case 'F':
 				System.out.println("Salary:");
-				fSalary = iScan.nextInt();
+				fSalary = scnr.nextInt();
 				System.out.println("Job Title:");
-				fTitle = sScan.nextLine();
-				// p.add(new Faculty(fullName, age, email, phoneNum, address, fSalary, fTitle));
+				fTitle = scnr.nextLine();
+				list.add(new Faculty(cTemp, fSalary, fTitle));
 				break;
 
 			default:
-				list.add(new Person(pName, pAge, pEmail, pPhoneNum, pAddress, pID));
-				list.add(new Person("Unknown", -1, "Unknown", -1, "Unknown", -1));
-				// list.add(person);
+				list.add(cTemp);
+				list.add(new Person("Unknown", -1, "Unknown", -1, "Unknown", pID));
+				// list.add(new Person(pName, pAge, pEmail, pPhoneNum, pAddress, pID));
 				break;
 			}
+
 			break;
 
 		case 'R':
 			found = false;
 			System.out.print("\nEnter ID : ");
-			dID = iScan.nextInt();
+			dID = scnr.nextInt();
 			while (iterList.hasNext()) {
 				Person pRetrieve = iterList.next();
 				if (pRetrieve.getID() == dID) {
@@ -119,18 +87,21 @@ public class AddressBookMain {
 		case 'U':
 			found = false;
 			System.out.print("\nEnter ID To Update: ");
-			dID = iScan.nextInt();
+			dID = scnr.nextInt();
 			ListIterator<Person> listIterator = list.listIterator();
 			while (listIterator.hasNext()) {
 				Person pUpdate = iterList.next();
 				if (pUpdate.getID() == dID) {
+					Person uTemp = createInput(found, list);
 					iterList.remove();
+					// listIterator.set(uTemp);
+					list.add(uTemp);
 					found = true;
 				}
 				if (!found) {
 					System.out.println("Person does not exist.");
 				} else if (found) {
-					System.out.println("Person with that ID has been deleted.");
+					System.out.println("Person with that ID has been Update.");
 					break;
 				}
 
@@ -140,7 +111,7 @@ public class AddressBookMain {
 		case 'D':
 			found = false;
 			System.out.print("\nEnter ID To Delete: ");
-			dID = iScan.nextInt();
+			dID = scnr.nextInt();
 			while (iterList.hasNext()) {
 				Person pDelete = iterList.next();
 				if (pDelete.getID() == dID) {
@@ -168,6 +139,47 @@ public class AddressBookMain {
 		default:
 			System.out.println("Please enter one of the option from the menu!");
 		}
+	}
+
+	public static Person createInput(boolean found, ArrayList<Person> list) {
+		Person asdf;
+		String pName = "\n";
+		String pEmail = "\n";
+		String pAddress = "\n";
+		long pPhoneNum = -1;
+		int pAge = -1;
+		int pID = -1;
+
+		Scanner scnr = new Scanner(System.in);
+
+		System.out.println("Please enter the following information:");
+
+		System.out.println("Full Name:");
+		pName = "pName"; // scnr.nextLine();
+
+		System.out.println("Age:");
+		pAge = -1;// scnr.nextInt();
+
+		do {
+			System.out.println("Email:");
+			pEmail = "p@email.com"; // scnr.next();
+			if (Person.checkEmail(pEmail) == false) {
+				System.out.println("Email is not correct format.");
+			}
+		} while (Person.checkEmail(pEmail) == false);
+
+		System.out.println("Phone Number:");
+		pPhoneNum = -1;// scnr.nextLine();
+
+		System.out.println("Home Address:");
+		pAddress = "pAddress";// scnr.nextLine();
+
+		System.out.println("ID Number:");
+		pID = scnr.nextInt();
+
+		asdf = new Person("Unknown", -1, "Unknown", -1, "Unknown", pID);
+
+		return asdf;
 	}
 
 	public static String menuOperationSelection() {
