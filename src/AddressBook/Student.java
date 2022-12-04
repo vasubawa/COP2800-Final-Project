21930005f88ea34e48232147437a539a34f48656
Dhruv <31012085+vasubawa@users.noreply.github.com>
2022-12-03 22:16:45 -0500

    Update Student.java

--- src/AddressBook/Student.java
+++ src/AddressBook/Student.java
@@ -1,11 +1,27 @@
 package AddressBook;
 
 public class Student extends Person {
-	/**
-	 * This class allows the user to input the GPA when the student type is
-	 * selected.
-	 */
-	public Student(Person createInput, float sGPA) {
+
+	private String id;
+
+	public Student(String pName, int pAge, String pEmail, String pPhoneNum, String pAddress, int pID, String sID) {
+		super(pName, pAge, pEmail, pPhoneNum, pAddress, pID);
+
+		setStudentID(sID);
+	}
+
+	public void setStudentID(String sID) {
+		this.id = sID;
+	}
+
+	public String getStudentID() {
+		return id;
+	}
+
+	public String toString() {
+		// super.toString();
+		// System.out.print(", Salary: " + salary + ", Title: " + title);
+		return super.toString() + ", Student ID: " + getStudentID();
 	}
 
 }
