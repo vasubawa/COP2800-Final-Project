7a2f17b2b751d7b15a48c80e2c91ef937ba6f81a
wabakee <90075954+wabakee@users.noreply.github.com>
2022-12-04 05:43:39 +0500

    Update Student.java
    Added class comments

--- src/AddressBook/Student.java
+++ src/AddressBook/Student.java
@@ -1,6 +1,10 @@
 package AddressBook;
+// Packages.
 
 public class Student extends Person {
+/**
+ * This class allows the user to input the GPA when the student type is selected. 
+ */
 
 	public Student(Person createInput, float sGPA) {
 	}
