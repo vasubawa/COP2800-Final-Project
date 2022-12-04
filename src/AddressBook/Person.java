da3f76da869d2d94a4399ff8cbc7431cf86aecee
Dhruv <31012085+vasubawa@users.noreply.github.com>
2022-12-03 21:08:20 -0500

    Update Person.java

--- src/AddressBook/Person.java
+++ src/AddressBook/Person.java
@@ -1,26 +1,27 @@
 package AddressBook;
-// Packages.
 
 public class Person {
-/**
- * This class allows the user to enter and stores information for each person 
- * including age, email, address, name, phone number, ID. 
- * @param A String array
- * @return A String representing a person's age, email, address, name, phone number, and ID.
- */
+	/**
+	 * This class allows the user to enter and stores information for each person
+	 * including age, email, address, name, phone number, ID.
+	 * 
+	 * @param A String array
+	 * @return A String representing a person's age, email, address, name, phone
+	 *         number, and ID.
+	 */
 	// Declared variables.
 	private int age;
 	private String email;
 	private String address;
 	private String name;
-	private long phoneNum;
+	private String phoneNum;
 	private int ID;
 
 	public Person() {
-		this("Unknown", -1, "Unknown", -1, "Unknown", -1);
+		this("Unknown", -1, "Unknown", "Unknown", "Unknown", -1);
 	}
 
-	public Person(String pName, int pAge, String pEmail, long pPhoneNum, String pAddress, int pID) {
+	public Person(String pName, int pAge, String pEmail, String pPhoneNum, String pAddress, int pID) {
 		setName(pName);
 		setAge(pAge);
 		setEmail(pEmail);
@@ -91,15 +92,14 @@ public class Person {
 		return name;
 	}
 
-	public void setPhoneNum(long pPhoneNum) {
+	public void setPhoneNum(String pPhoneNum) {
 		phoneNum = pPhoneNum;
 	}
 
-	public long getPhoneNum() {
+	public String getPhoneNum() {
 		return phoneNum;
 	}
 
-	@Override
 	public String toString() {
 		// return getName() + "\t" + getAge() + "\t" + getEmail() + "\t" + getPhoneNum()
 		// + "\t" + getAddress();
