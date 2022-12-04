f97d048a0a9d6f2a05d0336ce3d3ca8141b5ad75
Dhruv <31012085+vasubawa@users.noreply.github.com>
2022-12-03 21:09:07 -0500

    Update Faculty.java

--- src/AddressBook/Faculty.java
+++ src/AddressBook/Faculty.java
@@ -1,25 +1,39 @@
 package AddressBook;
-// Packages.
 
 public class Faculty extends Person {
-/**
- * This class allows the user to input the salary information when faculty 
- * type is selected when  creating a new person in the address book.
- **/
-	public Faculty(Person createInput, int fSalary, String fTitle) {
+	/**
+	 * This class allows the user to input the salary information when faculty type
+	 * is selected when creating a new person in the address book.
+	 **/
+	private double salary;
+	private String title;
+
+	public Faculty() {
+		this(-1, "Unknown");
 	}
 
-	/*
-	private double Salary;
-	
-	public final void setSalary(double value)
-	{
-		Salary = value;
-	}
-	public final double getSalary()
-	{
-		return Salary;
-	}
-	*/
+	public Faculty(double fSalary, String fTitle) {
 
+	}
+
+	public void setSalary(double fSalary) {
+		salary = fSalary;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setTitle(String fTitle) {
+		title = fTitle;
+	}
+
+	public double getTitle() {
+		return salary;
+	}
+
+	public void printAll() {
+		super.toString();
+		System.out.print(", Salary: " + salary + ", Title: " + title);
+	}
 }
